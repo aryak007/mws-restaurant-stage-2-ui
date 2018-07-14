0e80c1758b0568e1aa2677bdfd0abb24391d3eaf
Aryak Sengupta <aryak.sengupta@lexmark.com>
2018-07-14 23:45:05 +0530

    Added Access token. Initial commit

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiYXJ5YWsxOTkzIiwiYSI6ImNqamxnNnU1bDA3ZWIzcHMza3Z4b2d4c2oifQ.OO4dW1EH9m4Ry3n6wjH1Tg',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
